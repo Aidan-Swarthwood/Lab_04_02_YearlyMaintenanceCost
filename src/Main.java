20c1e01154347c688bfdc0df8d61099a8af31455
Aidan Swarthwood <amace10203@yahoo.com>
2023-09-17 21:38:45 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        //A program that uses home maintenance costs for each of the four seasons and returns the total yearly maintenance costs.
+        // Again, be clear in the output by echoing the values for the seasonal costs when you display the combined total.
+        int spring = 900;
+        int summer = 400;
+        int fall = 700;
+        int winter = 800;
+
+        int costOfAll = winter + spring + summer + fall;
+        System.out.println("The total yearly cost for maintenance is: " + costOfAll);
+
+
+
+    }
+}
